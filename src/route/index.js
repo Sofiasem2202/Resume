f4e5183d725a5b01ee175c6be58cb4a9f8766f64
Софія <sofialivin@gmail.com>
2023-07-25 14:04:16 +0200

    upd package

--- src/route/index.js
+++ src/route/index.js
@@ -451,7 +451,7 @@ router.get('/bio', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('bootstrap', function (req, res) {
+router.get('/bootstrap', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
